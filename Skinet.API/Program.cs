2061056e0ae5564c4c8af555c8e5f0e6007a41af
= <=>
2025-09-20 20:35:08 +0300

    add error handling

--- Skinet.API/Program.cs
+++ Skinet.API/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Skinet.API.Middlewares;
 using Skinet.Core.Interfaces;
 using Skinet.Infrastructure.Data;
 
@@ -20,11 +21,14 @@ namespace Skinet.API
 
             builder.Services.AddScoped<IProductRepository, ProductRepository>();
             builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+            builder.Services.AddCors();
 
             WebApplication app = builder.Build();
 
             // Configure the HTTP request pipeline.
-
+            app.UseMiddleware<ExceptionMiddleware>();
+            app.UseCors(opt => opt.AllowAnyHeader().AllowAnyMethod()
+                        .WithOrigins("https://localhost:4200", "http://localhost:4200"));
             app.MapControllers();
 
             try
