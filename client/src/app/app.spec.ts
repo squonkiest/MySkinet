81f325c31805c5a1d03c2d69d4fdf32fef132d99
= <=>
2025-09-21 15:40:47 +0300

    Change test titile in app.spec

--- client/src/app/app.spec.ts
+++ client/src/app/app.spec.ts
@@ -18,6 +18,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, client');
+    expect(compiled.querySelector('h1')?.textContent).toContain('client');
   });
 });
