2c7c35a3b7abab2cf0b284b93daa6a88184f149b
= <=>
2025-09-21 15:26:49 +0300

    Revert "test workflow"
    This reverts commit 6c2e08bbe1d6c2cffdcde92c893b80ecd4ed5fe9.

--- server/Skinet.API/Middlewares/ExceptionMiddleware.cs
+++ server/Skinet.API/Middlewares/ExceptionMiddleware.cs
@@ -20,7 +20,6 @@ namespace Skinet.API.Middlewares
 
         private static Task HandleExceptionAsync(HttpContext context, Exception ex, IHostEnvironment env)
         {
-            //test build
             context.Response.ContentType = "application/json";
             context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
 
